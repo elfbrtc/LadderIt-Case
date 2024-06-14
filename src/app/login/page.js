2f53a315fac96cd9b7d25772a47468d00c98a41e
elfbrtc <elif.barutcu.5353@hotmail.com>
2024-06-14 17:12:08 +0300

    -Update login page and add loading

--- src/app/login/page.js
+++ src/app/login/page.js
@@ -2,17 +2,28 @@
 
 import LoginForm from '../../components/LoginForm';
 import { useRouter } from 'next/navigation';
-import { useEffect } from 'react';
+import { useEffect, useState } from 'react';
 
 
 export default function LoginPage() {
   const router = useRouter();
+  const [isLoading, setIsLoading] = useState(true);
 
   useEffect(() => {    
     const isLoggedIn = localStorage.getItem('isLoggedIn');
     if(isLoggedIn)    
         router.push('/main');
+    else
+      setIsLoading(false);
   }, [router]);
+
+  if (isLoading) {
+    return (
+      <div className="loading-container">
+        <div>Loading...</div>
+      </div>
+    );
+  }
 
   return (
     <div>
